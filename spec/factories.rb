7e657f15d368687fa26bc2b5c2506201433e1269
Josh <Joshua.horgen@gmail.com>
2019-08-23 13:44:51 -0700

    Pass first two tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,23 @@
 FactoryBot.define do
+
+factory :business do
+  name { Faker::Movies::HarryPotter.character }
+  text_body { Faker::Movies::HarryPotter.spell }
+  location
+end
+
 factory :location do
-  city { Faker::Movies::HarryPotter.character }
-  state { Faker::Movies::HarryPotter.spell }
-  destination
+  city { Faker::Address.city }
+  state { Faker::Address.country }
+
+  factory :location_with_businesses do
+    transient do
+      businesses_count { 5 }
+    end
+
+    after(:create) do |location, evaluator|
+        create_list(:business, evaluator.businesses_count, location: location)
+      end
+    end
+  end
 end
