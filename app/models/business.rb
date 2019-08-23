d0c7ddce4e5fd1d4aa558282dc0ee82fc6dbf006
Josh <Joshua.horgen@gmail.com>
2019-08-23 13:05:47 -0700

    Add validations to models

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,7 @@
 class Business < ApplicationRecord
   belongs_to :location
 
+  validates :name, presence: true
+  validates :business_catagory, presence: true
+  validates :text_body, presence: true
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,6 @@
 class Location < ApplicationRecord
   has_many :businesses, dependent: :destroy
 
+  validates :city, presence: true
+  validates :state, presence: true
 end
