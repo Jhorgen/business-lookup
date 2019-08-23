e0b1c36bc1c000ce355c34d03f16c11ea1c242cd
Josh <Joshua.horgen@gmail.com>
2019-08-23 10:32:51 -0700

    Add CRUD/API functionality via controller.

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,41 @@
+class LocationsController < ApplicationController
+  before_action :authorize_request
+
+  def index
+    @locations = Location.all
+    json_response(@locations)
+  end
+
+  def show
+    @location = Location.find(params[:id])
+    json_response(@location)
+  end
+
+  def create
+    @location.create!(location_params)
+    json_response(@location)
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update(location_params)
+
+    end
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    if @location.destroy!
+
+    end
+  end
+
+  private
+  def location_params
+    params.permit(:city, :state)
+  end
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+end
